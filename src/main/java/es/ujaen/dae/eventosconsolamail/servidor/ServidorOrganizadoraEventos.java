83868594c3efa7725ba9e22f324ee333df0a79e4
dannyc0 <quar16@hotmail.com>
2018-10-25 17:16:48 +0200

    Avance conexion a base de datos

--- src/main/java/es/ujaen/dae/eventosconsolamail/servidor/ServidorOrganizadoraEventos.java
+++ src/main/java/es/ujaen/dae/eventosconsolamail/servidor/ServidorOrganizadoraEventos.java
@@ -2,12 +2,16 @@ package es.ujaen.dae.eventosconsolamail.servidor;
 
 import java.io.IOException;
 
+import javax.sql.DataSource;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.ImportResource;
+import org.springframework.jdbc.datasource.SingleConnectionDataSource;
 
 import es.ujaen.dae.eventosconsolamail.cliente.ClienteOrganizadoraEventos;
 
@@ -15,11 +19,25 @@ import es.ujaen.dae.eventosconsolamail.cliente.ClienteOrganizadoraEventos;
 @ComponentScan("es.ujaen.dae.eventosconsolamail.bean")
 @SpringBootApplication
 public class ServidorOrganizadoraEventos {
+	
+//	@Bean
+//	DataSource dataSource() {
+//		DataSource dataSource = new SingleConnectionDataSource();
+//		dataSource.setDriverClassName("org.apache.derby.jdbc.ClientDriver");
+//		dataSource.setUrl("jdbc:derby://localhost:1527/dae");
+//		dataSource.setUsername(“dae”);
+//		dataSource.setPassword(“dae”);
+//		return conexionBD;
+//	}
+	
+	
 	public static void main(String[] args) throws IOException {
 		SpringApplication servidor = new SpringApplication(ServidorOrganizadoraEventos.class);
 		ApplicationContext ctx = servidor.run(args);
 		
 		ClienteOrganizadoraEventos cliente = new ClienteOrganizadoraEventos(ctx);
 		cliente.run();
+		
+		
 	}
 }
