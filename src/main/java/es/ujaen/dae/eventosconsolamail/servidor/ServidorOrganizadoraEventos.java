aa6d6b412c821a1b0013fe3a4eeba8f76db19cde
dannyc0 <quar16@hotmail.com>
2018-10-30 20:50:48 +0100

    JPA funcionando al fin

--- src/main/java/es/ujaen/dae/eventosconsolamail/servidor/ServidorOrganizadoraEventos.java
+++ src/main/java/es/ujaen/dae/eventosconsolamail/servidor/ServidorOrganizadoraEventos.java
@@ -10,6 +10,7 @@ import org.springframework.boot.context.properties.EnableConfigurationProperties
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.PropertySource;
 
 import es.ujaen.dae.eventosconsolamail.cliente.ClienteOrganizadoraEventos;
 import es.ujaen.dae.eventosconsolamail.dao.EventoDAO;
@@ -19,17 +20,13 @@ import es.ujaen.dae.eventosconsolamail.modelo.Usuario;
 @EnableAutoConfiguration
 @EnableConfigurationProperties
 @ComponentScan("es.ujaen.dae.eventosconsolamail.bean")
-@EntityScan(basePackages= {"es.ujaen.dae.eventosconsolamail.bean.modelo"})
+@EntityScan(basePackages= {"es.ujaen.dae.eventosconsolamail.modelo"})
 @SpringBootApplication
 public class ServidorOrganizadoraEventos {
 	
 	public static void main(String[] args) throws IOException {
 		SpringApplication servidor = new SpringApplication(ServidorOrganizadoraEventos.class);
 		ApplicationContext ctx = servidor.run(args);
-		
-		EventoDAO evento = new EventoDAO(); 
-		
-		evento.insertar(new Evento("evento","aa","aa","aa","aa",1,new Usuario()));
 		
 		ClienteOrganizadoraEventos cliente = new ClienteOrganizadoraEventos(ctx);
 		cliente.run();	
